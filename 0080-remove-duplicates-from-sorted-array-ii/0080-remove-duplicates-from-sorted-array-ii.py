2faee57d420b776dfafb25899f15d19dc70e1388
hestia-park <124057808+hestia-park@users.noreply.github.com>
2024-10-12 15:05:05 +0900

    Time: 54 ms (39.9%), Space: 16.5 MB (55.98%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -3,7 +3,7 @@ class Solution:
         if len(nums) <= 2:
             return len(nums)
         
-        j = 2  # Start from index 2 as first two elements are always allowed
+        j = 2  
         for i in range(2, len(nums)):
             if nums[i] != nums[j - 2]:
                 nums[j] = nums[i]
