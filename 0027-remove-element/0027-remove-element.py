949bc741af603ff1ba7d19907d5b8d24c0a98268
hestia-park <124057808+hestia-park@users.noreply.github.com>
2024-10-12 14:06:58 +0900

    Time: 45 ms (6.92%), Space: 16.5 MB (31.44%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,7 +1,5 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # first code
-        # it will take a O(n^2) because of pop()
         i=0
         while i <len(nums) :
             if nums[i]==val:
