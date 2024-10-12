aa9b1ec1af5cb596825451ecbe906e1250a5cb51
hestia-park <124057808+hestia-park@users.noreply.github.com>
2024-10-12 15:16:50 +0900

    Time: 179 ms (23.41%), Space: 18 MB (84.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hit=[0]*(len(nums)+1)
+        hit={}
         # print(hit)
         for i in nums:
-            hit[i] +=1 
-        return hit.index(max(hit))
+            if not i in hit:
+                hit[i]=1
+            else:
+                hit[i]+=1
+
+        return max(hit, key=hit.get)
