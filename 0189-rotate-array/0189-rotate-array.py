8cb9d0e500d7c4b1b5f4bd8688ffed3555976c2b
hestia-park <124057808+hestia-park@users.noreply.github.com>
2024-10-12 15:55:28 +0900

    Time: 162 ms (44.82%), Space: 28.1 MB (36.49%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,17 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #165 ms,Beats 34.86%
         # print(len(nums))
-        k=k%len(nums)
-        j=k-1
-        output=nums.copy()
-        for i in range(k):
-            nums[j-i]=output[len(nums)-1-i]
-            # print(i,j-i,nums,output)
-            # j+=1
-        for i in range(k,len(nums)):
-            # print(i,i-j,nums,output)
-            nums[i]=output[i-j-1]
+        # k=k%len(nums)
+        # j=k-1
+        # output=nums.copy()
+        # for i in range(k):
+        #     nums[j-i]=output[len(nums)-1-i]
+        #     # print(i,j-i,nums,output)
+        #     # j+=1
+        # for i in range(k,len(nums)):
+        #     # print(i,i-j,nums,output)
+        #     nums[i]=output[i-j-1]
+        k = k % len(nums)
+        def reverse(l, r, nums):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l, r = l + 1, r - 1
+
+        reverse(0, len(nums) - 1, nums)
+        reverse(0, k - 1, nums)
+        reverse(k, len(nums) - 1, nums)
 
 
         
