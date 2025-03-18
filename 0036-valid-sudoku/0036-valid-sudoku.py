cf4bcbbc88ff5503f5386938fa9eb32b0e419763
hestia-park <124057808+hestia-park@users.noreply.github.com>
2025-03-19 00:34:05 +0900

    Time: 4 ms (60.57%), Space: 17.9 MB (44.12%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,31 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-            # Check rows
-        for row in board:
-            seen = set()
-            for num in row:
-                if num != '.' and num in seen:
-                    return False
-                seen.add(num)
-        
-        # Check columns
-        for col in range(9):
-            seen = set()
-            for row in range(9):
-                num = board[row][col]
-                if num != '.' and num in seen:
-                    return False
-                seen.add(num)
+        # Check rows and columns
+        for i in range(9):
+            row_set = set()
+            col_set = set()
+            for j in range(9):
+                # Row check
+                if board[i][j] != ".":
+                    if board[i][j] in row_set:
+                        return False
+                    row_set.add(board[i][j])
+                
+                # Column check
+                if board[j][i] != ".":
+                    if board[j][i] in col_set:
+                        return False
+                    col_set.add(board[j][i])
         
         # Check 3x3 sub-boxes
         for box_row in range(0, 9, 3):
             for box_col in range(0, 9, 3):
-                seen = set()
-                for row in range(box_row, box_row + 3):
-                    for col in range(box_col, box_col + 3):
-                        num = board[row][col]
-                        if num != '.' and num in seen:
-                            return False
-                        seen.add(num)
+                box_set = set()
+                for i in range(3):
+                    for j in range(3):
+                        num = board[box_row + i][box_col + j]
+                        if num != ".":
+                            if num in box_set:
+                                return False
+                            box_set.add(num)
         
         return True
